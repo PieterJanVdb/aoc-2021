5b1ee78dc03372e7ba118eb587d95bee5400f226
Pieter-Jan Vandenbussche <pj.vandenbussche@gmail.com>
2021-12-09 01:46:35 +0100

    Combine part 1 & 2 into a single fn

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -11,32 +11,20 @@ fn get_moves() -> Vec<(String, i32)> {
         .collect()
 }
 
-fn calculate_position_1(moves: &[(String, i32)]) -> i32 {
-    let (horizontal, depth) = moves.iter().fold((0, 0), |acc, x| match x.0.as_str() {
-        "forward" => (acc.0 + x.1, acc.1),
-        "down" => (acc.0, acc.1 + x.1),
-        "up" => (acc.0, acc.1 - x.1),
-        _ => acc,
-    });
-
-    horizontal * depth
-}
-
-fn calculate_position_2(moves: &[(String, i32)]) -> i32 {
-    let (horizontal, depth, _) = moves.iter().fold((0, 0, 0), |acc, x| match x.0.as_str() {
+fn calculate_position(moves: &[(String, i32)]) -> (i32, i32) {
+    let (horizontal, depth, aim) = moves.iter().fold((0, 0, 0), |acc, x| match x.0.as_str() {
         "forward" => (acc.0 + x.1, acc.1 + x.1 * acc.2, acc.2),
         "down" => (acc.0, acc.1, acc.2 + x.1),
         "up" => (acc.0, acc.1, acc.2 - x.1),
         _ => acc,
     });
 
-    horizontal * depth
+    (horizontal * aim, horizontal * depth)
 }
 
 fn main() {
     let moves = get_moves();
-    let result_1 = calculate_position_1(&moves);
-    let result_2 = calculate_position_2(&moves);
+    let (result_1, result_2) = calculate_position(&moves);
 
     println!("Part 1: {}", result_1);
     println!("Part 2: {}", result_2);
